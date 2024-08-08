0084b9fd448e46a97e18964e6ec19dcae0890e18
name <email>
2024-08-08 11:09:29 +0200

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+import Home from './screens/Home';
+import Game from './screens/Game'
+import Result from './screens/Result';
+import { HeaderBackButton } from '@react-navigation/elements';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style='auto' />
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="Game" component={Game}/>
+        <Stack.Screen
+          name='Result'
+          component={Result}
+          options={({ navigation }) => {
+            headerLeft: (props) => {
+              <HeaderBackButton
+                {...props}
+                label='Home'
+                onPress={() => 
+                  navigation.navigate('Home')}
+              />
+            }
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/Game.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, Button, View, StatusBar, Alert, TouchableHighlight } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import AnimatedButton from '../components/AnimatedButton';
+
+export default function Game() {
+    
+    const [choice, setChoice] = useState('');
+    const baseNumber = Math.floor(Math.random() * 100);
+    const score = Math.floor(Math.random() * 100);
+
+    console.log(`beginning base number = ${baseNumber} et score = ${score}`);
+
+    const navigation = useNavigation();
+    useEffect(() => {
+
+        if (choice) {
+            console.log(` base Number = ${baseNumber} et score = ${score} `);
+            const winner = 
+                (choice === 'higher' && score > baseNumber) ||
+                (choice === 'lower' && baseNumber > score);
+            Alert.alert(`You've ${winner ? 'won' : 'lost'}`,
+                `You scored: ${score}`);
+            navigation.goBack();
+        }
+    }, [baseNumber, score, choice])
+
+    return (
+        <View style={styles.container}>
+            <StatusBar barStyle='dark-content'/>
+            <Text style={styles.baseNumber}>Starting : {baseNumber}</Text>
+            <AnimatedButton action='higher' onPress={() => setChoice('higher')} />
+            <AnimatedButton action='lower' onPress={() => setChoice('lower')} />
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    baseNumber: {
+        fontSize: 48,
+        marginBottom: 30,
+    },
+});
